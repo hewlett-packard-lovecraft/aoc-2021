26e926efa8baa466d65a9fd870a54a8606ede73c
Haoming Xia <mrmist3r311@gmail.com>
2021-12-05 22:03:52 -0800

    day4

---
+++ day4/day4.py
@@ -0,0 +1,7 @@
+# 1. take input, split input by /n/n
+# 2. 1st line is special, put into aray nums
+# 3. spli rest of input into grids
+# 4. go through num, check each grid for num
+# 5. check for win condition
+# 6. repeat step 4-5
+
