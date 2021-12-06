df30ce9eebf7f43a2ebc26efb4dcf911246e2b28
Haoming Xia <mrmist3r311@gmail.com>
2021-12-06 00:57:06 -0800

    day4

--- day4/day4.py
+++ day4/day4.py
@@ -5,3 +5,46 @@
 # 5. check for win condition
 # 6. repeat step 4-5
 
+def has_won(grid, called_out: list[int]):
+    for row in grid:  # check rows
+        if all(x in called_out for x in row):
+            return True  # return true if all nums in row have been called out
+
+    for row_index in range(len(grid[0])):
+        if all(x[row_index] in called_out for x in grid):
+            return True  # return true if all nums in col have been called out
+
+
+def uncalled_numbers_sum(grid, called_out):
+    all_nums = set()
+    for row in grid:
+        for x in row:
+            all_nums.add(x)
+    
+    return sum(all_nums - set(called_out))
+
+
+if __name__ == '__main__':
+    grids = []
+    nums = []
+    called_out = []
+
+    for board in open('day4/input_test.txt').read().split('\n\n'):
+        grid = []
+        if ',' in board:
+            nums = board.split(',')  # first line is special
+        else:
+            for line in board.splitlines():
+                row = list(map(int, line.split()))
+                grid.append(row)
+
+        grids.append(grid)
+    
+    grids.pop(0)
+    
+    for i, num in enumerate(nums):
+        called_out.append(num)
+        for grid in grids:
+            if has_won(grid, called_out):
+                print(uncalled_numbers_sum(grid, called_out))
+                break
